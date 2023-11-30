54c4974274c1e880233a6becc9dc375037702abd
Jordan Dearsley <jordan@dearsley.dev>
2023-11-30 16:33:36 -0500

    Checking in

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jordan.cde/vapi_python',
-    version='0.1.6',
+    version='0.1.7',
     zip_safe=False,
 )
